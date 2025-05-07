41c178a4ae7e4028ab6990798d600be301f19826
Bryan Stitt <bryanstitt99@gmail.com>
2025-05-07 18:56:20 -0500

    update

--- main.py
+++ main.py
@@ -67,11 +67,11 @@ Selenium setup
 '''
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-service = Service('C:\\Users\\bryan\\chromedriver-win64\\chromedriver.exe')
-# service = Service('/usr/bin/chromedriver')
+# service = Service('C:\\Users\\bryan\\chromedriver-win64\\chromedriver.exe')
+service = Service('/usr/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 sudo_driver = webdriver.Chrome(service=service, options=chrome_options)
 
