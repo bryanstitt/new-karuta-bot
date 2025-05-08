e8a70a2a82c3414b7f1deaf5ee28d380531c3213
ryandis44 <ryandisanti5810@gmail.com>
2025-05-07 19:11:24 -0500

    added API integration for bot access lists, including sudo permissions

--- API/API.py
+++ API/API.py
@@ -37,7 +37,8 @@ class KGBAPI:
     
     
     
-    def get_self(self) -> dict|None: return self.get_bot(bot_id = BOT_ID)
+    @property
+    def me(self) -> dict|None: return self.get_bot(bot_id = BOT_ID)
 
 
     
@@ -60,3 +61,25 @@ class KGBAPI:
         response = requests.get(url, headers=self.headers)
         if response.status_code == 200: return response.json()
         else: raise Exception(f"API Error: {response.status_code} - {response.text}")
+
+    
+    
+    def get_bot_access_list(self, bot = None) -> dict:
+        '''
+        Get access list for this bot
+        (Who can do what with this bot)
+        '''
+        if not bot: bot = self.me
+        return bot['access_list']
+    
+    
+    
+    def can_i_sudo(self, user_id: int) -> bool:
+        '''
+        Helper function that returns bool
+        of whether or not a user can sudo
+        a particular bot.
+        '''
+        acl = self.get_bot_access_list()
+        if acl.get(str(user_id)) and acl[str(user_id)] in ["OWNER", "DEVELOPER"]: return True
+        return False
